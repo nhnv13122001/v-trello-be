e99766d10cbc72060678e7d4fd612314671ea781
nhnv13122001 <nhnv13122001@gmail.com>
2024-11-14 21:05:53 +0700

    Add basic controllers

--- src/validations/boardValidation.js
+++ src/validations/boardValidation.js
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 import { StatusCodes } from 'http-status-codes'
 
-const createNew = async (req, res) => {
+const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
     title: Joi.string().required().min(3).max(50).trim().strict().messages({
       'any.required': 'Title is required',
@@ -30,13 +30,10 @@ const createNew = async (req, res) => {
 
   try {
     await correctCondition.validateAsync(req.body, { abortEarly: false })
-
-    res.status(StatusCodes.CREATED).json({
-      message: 'POST from Validation: API create new board'
-    })
+    next()
   } catch (error) {
     res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
-      error: new Error(error).message
+      errors: new Error(error).message
     })
   }
 }
