c16196e4b6e72c4218b7e4d74fed6c4d528bc3b1
nhnv13122001 <nhnv13122001@gmail.com>
2024-11-28 22:35:48 +0700

    Complete applying Brevo to send a verification email

--- src/services/userService.js
+++ src/services/userService.js
@@ -6,13 +6,16 @@ import { StatusCodes } from 'http-status-codes'
 import MyError from '~/utils/MyError'
 import { pickUser } from '~/utils/formatter'
 import { userModel } from '~/models/userModel'
+import { WEBSITE_DOMAIN } from '~/utils/constants'
+import { brevoProvider } from '~/providers/brevoProvider'
 
 const createNew = async (reqBody) => {
   try {
     const existUser = await userModel.findOneByEmail(reqBody.email)
     if (existUser) {
-      throw new MyError(StatusCodes.CONFLICT, 'Email already existsssss!')
+      throw new MyError(StatusCodes.CONFLICT, 'Email already exists!')
     }
+
     const nameFromEmail = reqBody.email.split('@')[0]
     const newUser = {
       email: reqBody.email,
@@ -23,6 +26,17 @@ const createNew = async (reqBody) => {
     }
     const createdUser = await userModel.createNew(newUser)
     const getNewUser = await userModel.findOneById(createdUser.insertedId)
+
+    const verificationLink = `${WEBSITE_DOMAIN}/account/verification?email=${getNewUser.email}&token=${getNewUser.verifyToken}`
+    const subject = 'Trello: Please verify your email before using our services'
+    const htmlContent = `
+    <h3>Here is your verification link:</h3>
+    <h3>${verificationLink}</h3>
+    <h3>Sincerely,<br/> - Nhnv13122001 - </h3>
+    `
+
+    await brevoProvider.sendEmail(getNewUser.email, subject, htmlContent)
+
     return pickUser(getNewUser)
   } catch (error) {
     throw error
